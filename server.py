90426da152cdce4012f63f62925f2fb8244724de
SemyonGulyaev <gusa@mail.ru>
2021-05-16 18:19:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello this is the new version!!!"
+
+@app.route("/json")
+def getJSON():
+    return jsonify({'name':'Semyon',
+                    'email':'noc77@gmail.com',
+                    'locale':'https://youtube.com'})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, threaded=True)
