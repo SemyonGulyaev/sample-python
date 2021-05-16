4be73a297301eedb77c8644af778a1804b5cbb90
SemyonGulyaev <gusa@mail.ru>
2021-05-16 18:30:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,5 +12,10 @@ def getJSON():
                     'email':'noc77@gmail.com',
                     'locale':'https://youtube.com'})
 
+@app.route("/routers", methods=['GET'])
+def getRouter():
+    hostname = request.args.get('hostname')
+    return hostname
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, threaded=True)
